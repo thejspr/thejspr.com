1b1d0e163346185c491facfff56cfb71ecb29613
Jesper Kjeldgaard <thejspr@gmail.com>
2021-11-03 16:09:31 +0100

    update index

--- _plugins/tag_hook.rb
+++ _plugins/tag_hook.rb
@@ -1,16 +1,16 @@
 Jekyll::Hooks.register :posts, :post_write do |post|
-  all_existing_tags = Dir.entries("tags")
-    .map { |t| t.match(/(.*).html/) }
-    .compact.map { |m| m[1] }
+  all_existing_tags = Dir.entries('tags')
+                         .map { |t| t.match(/(.*).html/) }
+                         .compact.map { |m| m[1] }
 
-  tags = post['tags'].reject { |t| t.empty? }
+  tags = post['tags'].reject(&:empty?)
   tags.each do |tag|
-    generate_tag_file(tag) if !all_existing_tags.include?(tag)
+    generate_tag_file(tag) unless all_existing_tags.include?(tag)
   end
 end
 
 def generate_tag_file(tag)
-  File.open("tags/#{tag}.html", "wb") do |file|
+  File.open("tags/#{tag}.html", 'wb') do |file|
     file << "---\nlayout: tags\ntag-name: #{tag}\ntitle: Posts tagged #{tag}\n---\n\n"
   end
 end
